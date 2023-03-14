c2ce64c1316ebe0d4a65e5fdb13f61c6c687f142
Joshua Watson <joshjkwatson@gmail.com>
2023-03-14 22:10:42 +1100

    updated java

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,31 +1,19 @@
 // Variables
-// var container = document.querySelector("quiz-container");
+var container = document.getElementById("quiz-container");
 // var quizQuestions = document.querySelector("quizQuestions");
 var timerEl = document.getElementById('countdown');
 var mainEl = document.getElementById('main');
 var startQuizEl = document.querySelector('#startQuiz');
-var questionEl = document.getElementById('question')
+var questionEl = document.getElementById('question');
 // startGameButton.addEventListener("click"); 
-
+var startBtn = document.getElementById('startQuiz');
+var currentIndex = 0;
 // quizQuestions.style.display = 'hidden';
 
 var start = true;
 
-function startQuiz() {
-    console.log(startQuiz)
-    quizQuestions.style.display = 'visible';
-    timerCount = 60;
-    startQuiz.disable = true;
-    questionEl.textContent = (quizQuestions)
-}
-
-startQuizEl.addEventListener("click", startQuiz);
- 
-function displayQuestion(){
-    
-}
-
- var quizQuestions = [{
+var quizQuestions = [
+    {
 
     id: 0,
     question: "What is 2+2?",
@@ -60,16 +48,90 @@ function displayQuestion(){
     
 ];
 
+
+
+function startQuiz() {
+    // var startQuiz = document.getElementById("startScreen");
+    container.removeAttribute("class");
+    console.log(startQuiz);
+    timerCount = 60;
+    startQuiz.disable = true;
+    
+}
+
+
+ 
+function displayQuestion(){
+    var currentQuestion = quizQuestions[currentIndex];
+
+}
+
+ 
+
 console.log(quizQuestions);
 
 var answers = document.getElementsByClassName('answers')
 
-function onClickedAnswer(answers) {
-    if (answers.addEventListener('click'))
-    displayQuestion();
+// function onClickedAnswer(answers) {
+//     if (answers.addEventListener('click'))
+//     displayQuestion();
     
-};
+// };
     
+function getQuestion() {
+    // get current question object from array
+    var currentQuestion = questions[currentIndex];
+    // update title with current question
+    var titleEl = document.getElementById("question-title");
+    titleEl.textContent = currentQuestion.title;
+    // clear out any old question choices
+    choicesEl.innerHTML = "";
+    // loop over choices
+    currentQuestion.choices.forEach(function(choice, i) {
+      // create new button for each choice
+      var choiceNode = document.createElement("button");
+      choiceNode.setAttribute("class", "choice");
+      choiceNode.setAttribute("value", choice);
+      choiceNode.textContent = i + 1 + ". " + choice;
+      // attach click event listener to each choice
+      choiceNode.onclick = questionClick;
+      // display on the page
+      choicesEl.appendChild(choiceNode);
+    });
+  }
+
+    function questionClick() {
+    // check if user guessed wrong
+    if (this.value !== questions[currentQuestionIndex].answer) {
+      // penalize time
+      time -= 15;
+      if (time < 0) {
+        time = 0;
+      }
+      // display new time on page
+      timerEl.textContent = time;
+      // play "wrong" sound effect
+      sfxWrong.play();
+      feedbackEl.textContent = "Wrong!";
+    } else {
+      // play "right" sound effect
+      sfxRight.play();
+      feedbackEl.textContent = "Correct!";
+    }
+    // flash right/wrong feedback on page for half a second
+    feedbackEl.setAttribute("class", "feedback");
+    setTimeout(function() {
+      feedbackEl.setAttribute("class", "feedback hide");
+    }, 1000);
+    // move to next question
+    currentQuestionIndex++;
+    // check if we've run out of questions
+    if (currentQuestionIndex === questions.length) {
+      quizEnd();
+    } else {
+      getQuestion();
+    }
+  }
 
 
 // this is the function that occurs when startQuiz is clicked
@@ -168,6 +230,7 @@ var msgInterval = setInterval(function (){
 console.log (countdown) 
 countdown();
 
+startBtn.addEventListener("click", startQuiz);
 
 
 
