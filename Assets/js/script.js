adb97c8f0e3842ed9e6396137ec471835a6aa5f3
josh Watson <joshjkwatson@gmail.com>
2023-03-16 18:57:25 +1100

    Javaupdate

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,18 +4,20 @@ var container = document.getElementById("quiz-container");
 var timerEl = document.getElementById('countdown');
 var mainEl = document.getElementById('main');
 var startQuizEl = document.querySelector('#startQuiz');
-var questionEl = document.getElementById('question');
+// var questionEl = document.getElementById('question');
 // startGameButton.addEventListener("click"); 
 var startBtn = document.getElementById('startQuiz');
-var currentIndex = 0;
+var questionEl = document.getElementById('question');
+var currentQuestionIndex = 0;
+var gameBtns = document.getElementsByClassName('options-container');
 // quizQuestions.style.display = 'hidden';
 
 var start = true;
 
 var quizQuestions = [
+    
     {
 
-    id: 0,
     question: "What is 2+2?",
     answer: [
         {text: "3", isCorrect: false },
@@ -26,7 +28,6 @@ var quizQuestions = [
     },
     {  
 
-    id: 1, 
     question: "What is the capital city of Australia?",
     answer: [
         {text: "Brisbane", isCorrect: false },
@@ -36,7 +37,7 @@ var quizQuestions = [
     ]
     },
     {   
-        id: 2,
+        
     question: "What is 10 + 12?",
     answer: [
         {text: "22", isCorrect: true},
@@ -59,15 +60,6 @@ function startQuiz() {
     
 }
 
-
- 
-function displayQuestion(){
-    var currentQuestion = quizQuestions[currentIndex];
-
-}
-
- 
-
 console.log(quizQuestions);
 
 var answers = document.getElementsByClassName('answers')
@@ -80,54 +72,53 @@ var answers = document.getElementsByClassName('answers')
     
 function getQuestion() {
     // get current question object from array
-    var currentQuestion = questions[currentIndex];
+    var currentQuestion = quizQuestions[currentQuestionIndex];
     // update title with current question
-    var titleEl = document.getElementById("question-title");
-    titleEl.textContent = currentQuestion.title;
+    var questionEl = document.getElementsByClassName("questions-container");
+    questionEl.textContent = currentQuestion.title;
     // clear out any old question choices
-    choicesEl.innerHTML = "";
+    questionEl.innerHTML = "";
     // loop over choices
-    currentQuestion.choices.forEach(function(choice, i) {
-      // create new button for each choice
-      var choiceNode = document.createElement("button");
-      choiceNode.setAttribute("class", "choice");
-      choiceNode.setAttribute("value", choice);
-      choiceNode.textContent = i + 1 + ". " + choice;
+    currentQuestion.answer.forEach(function(answers, i) {
+      // // create new button for each choice
+      // var choiceNode = document.getElementById("answers");
+      // choiceNode.setAttribute("id", "answers");
+      // choiceNode.setAttribute("value", choice);
+      // choiceNode.textContent = i + 1 + ". " + choice;
       // attach click event listener to each choice
-      choiceNode.onclick = questionClick;
+      gameBtns.onClick = questionClick();
       // display on the page
-      choicesEl.appendChild(choiceNode);
+      questionEl.appendChild(choiceNode)
     });
   }
 
     function questionClick() {
     // check if user guessed wrong
-    if (this.value !== questions[currentQuestionIndex].answer) {
-      // penalize time
-      time -= 15;
-      if (time < 0) {
-        time = 0;
-      }
-      // display new time on page
-      timerEl.textContent = time;
-      // play "wrong" sound effect
-      sfxWrong.play();
-      feedbackEl.textContent = "Wrong!";
-    } else {
-      // play "right" sound effect
-      sfxRight.play();
-      feedbackEl.textContent = "Correct!";
-    }
+  // if (this.value !== questions[currentQuestionIndex].answer) {
+  //     // penalize time
+  //     // time -= 15;
+  //     // if (time < 0) {
+  //     //   time = 0;
+  //     // }
+  //     // display new time on page
+  //     timerEl.textContent = time;
+  //   //   // play "wrong" sound effect
+  //   //   sfxWrong.play();
+  //   //   feedbackEl.textContent = "Wrong!";
+  //   // } else {
+  //   //   // play "right" sound effect
+  //   //   sfxRight.play();
+  //   //   feedbackEl.textContent = "Correct!";
+  //   }
     // flash right/wrong feedback on page for half a second
-    feedbackEl.setAttribute("class", "feedback");
-    setTimeout(function() {
-      feedbackEl.setAttribute("class", "feedback hide");
-    }, 1000);
+    // feedbackEl.setAttribute("class", "feedback");
+    // setTimeout(function() {
+    //   feedbackEl.setAttribute("class", "feedback hide");
+    // }, 1000);
     // move to next question
     currentQuestionIndex++;
     // check if we've run out of questions
-    if (currentQuestionIndex === questions.length) {
-      quizEnd();
+    if (currentQuestionIndex === quizQuestions.length) {
     } else {
       getQuestion();
     }
